e63cf891cf029f50b6d17dcf8086b0ea0b42fbed
davidvhill <davehill75@gmail.com>
2016-10-04 21:12:22 -0500

    updated app.py logging configuration to goto stdout

--- ccd/app.py
+++ ccd/app.py
@@ -2,20 +2,38 @@
 requires configuration or services should import app and obtain the
 configuration or service from here.
 
-This pattern is borrowed from flask.
+app.py enables a very basic but sufficient form of loose coupling
+by setting names of services & configuration once and allowing other modules
+that require these services/information to obtain them by name rather than
+by directly importing or instantiating.
+
+Module level constructs are only evaluated once in a Python application's
+lifecycle, usually at the time of first import. This pattern is borrowed
+from Flask.
 """
+import logging
+import sys
+
+############################
 # Logging system
+############################
 # to use the logging from any module:
 # import app
 # logger = app.logging.getLogger(__name__)
 #
 # To alter where log messages go or how they are represented, configure the
 # logging system below.
-import logging
 # iso8601 date format
-logging.basicConfig(format='%(asctime)s %(message)s',
+
+logging.basicConfig(stream=sys.stdout,
+                    level=logging.DEBUG,
+                    format='%(asctime)s %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
 
+
+############################
+# Global configuration items
+############################
 MINIMUM_CLEAR_OBSERVATION_COUNT = 12
 
 # 2 for tri-modal; 2 for bi-modal; 2 for seasonality; 2 for linear
